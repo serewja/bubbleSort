7165f54734327c21bc21b0eb1d9d1e03b6e1a68a
Sergei Shinkarenko <serewja@gmail.com>
2020-07-13 22:24:10 +0500

    add test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,7 @@
 import f from "../dist/index.js"
 
 test('testname', () => {
-    
+  expect(bubbleSort([])).toEqual([]);
+  expect(bubbleSort([10, 1, 3])).toEqual([1, 3, 10]);
+  expect(bubbleSort([0, 4, 0, 10, -3])).toEqual([-3, 0, 0, 4, 10]);
 });
